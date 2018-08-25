e3a3dc4a47f6bde35d9179aca298aaffd3967302
Steven Dao <stevedao@gmail.com>
2018-08-25 11:38:20 -0700

    update animation

--- anim.js
+++ anim.js
@@ -2,44 +2,24 @@ window.onload = function() {
     var scene = new THREE.Scene();
     scene.background = new THREE.Color( 0xf6f6f6 );
 
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 
-    var renderer = new THREE.WebGLRenderer( {antialias: true} );
+    var renderer = new THREE.WebGLRenderer({antialias: true});
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.getElementById("anim").appendChild( renderer.domElement );
+    document.getElementById("anim").appendChild(renderer.domElement);
 
-    var geometry = new THREE.WireframeGeometry( new THREE.SphereGeometry( 5, 24, 24 ) );
-    var material = new THREE.LineBasicMaterial( {
-        color: 0x6699cc,
-        linewidth: 1,
-        opacity: 0.7,
-        transparent: true,
-    } );
-    var line = new THREE.LineSegments( geometry, material );
+    var geometry = new THREE.SphereGeometry(5, 10, 10);
+    var material = new THREE.MeshStandardMaterial({
+        color: 0xffffff,
+        flatShading: true,
+        metalness: 0.1,
+        side: THREE.BackSide
+    });
+    var mesh = new THREE.Mesh(geometry, material);
+    scene.add( mesh );
 
-    var geometry1 = new THREE.WireframeGeometry( new THREE.SphereGeometry( 6, 32, 32 ) );
-    var material1 = new THREE.LineBasicMaterial( {
-        color: 0xff6688,
-        linewidth: 1,
-        opacity: 0.6,
-        transparent: true,
-    } );
-    var line1 = new THREE.LineSegments( geometry1, material1 );
-
-    var geometry2 = new THREE.WireframeGeometry( new THREE.SphereGeometry( 7, 16, 16 ) );
-    var material2 = new THREE.LineBasicMaterial( {
-        color: 0x66cc99,
-        linewidth: 1,
-        opacity: 0.8,
-        transparent: true,
-    } );
-    var line2 = new THREE.LineSegments( geometry2, material2 );
-    line2.position.x = -2.0;
-
-    scene.add( line );
-    scene.add( line1 );
-    scene.add( line2 );
-
+    var light = new THREE.HemisphereLight(0x3366cc, 0x99ffdd, 1 );
+    scene.add( light );
     camera.position.z = 5;
 
     window.addEventListener( 'resize', onWindowResize, false );
@@ -49,19 +29,17 @@ window.onload = function() {
         camera.fov = Math.min((window.innerHeight / window.innerWidth) * 220.0, 70.0);
         camera.updateProjectionMatrix();
 
-        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.setSize(window.innerWidth, window.innerHeight);
     }
 
     var animate = function (time) {
-        line.rotation.x = time / 10000.0;
-        line1.rotation.y = time / 12000.0;
-        line2.rotation.z = time / 8000.0;
-        line1.position.x = Math.sin(time / 3000.0) * 2.0;
-        line2.position.x = Math.cos(time / 4000.0) * 1.0;
+        mesh.rotation.x = time / 12000.0;
+        mesh.rotation.y = time / 8000.0;
+        mesh.rotation.z = time / 24000.0;
 
         renderer.render(scene, camera);
 
-        requestAnimationFrame( animate );
+        requestAnimationFrame(animate);
     };
 
     onWindowResize();
